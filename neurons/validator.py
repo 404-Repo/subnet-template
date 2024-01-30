a4a67e308276507de0bf16744a8d53ebe556fb5d
Maxim Bladyko <bladyko@gmail.com>
2024-01-30 09:05:02 +0300

    Validation code added

--- neurons/validator.py
+++ neurons/validator.py
@@ -4,14 +4,19 @@ from typing import List
 import bittensor as bt
 import torch
 from config import read_config
+from validating import Validate3DModels, load_models, score_responses
 from protocol import TextTo3D
 
 from neurons.base_validator import BaseValidatorNeuron
 
 
 class Validator(BaseValidatorNeuron):
+    models: Validate3DModels
+
     def __init__(self, config: bt.config):
         super(Validator, self).__init__(config)
+
+        self.models = load_models(self.device, config.neuron.full_path)
 
     async def forward(self):
         """
@@ -24,31 +29,24 @@ class Validator(BaseValidatorNeuron):
         """
         miner_uids = self.get_random_miners_uids(self.config.neuron.sample_size)
 
-        # TODO: move probe creation to a separate module
+        # TODO: get prompts from the dataset
+        prompt = "A Golden Poison Dart Frog"
+
         responses = await self.dendrite.forward(
             axons=[self.metagraph.axons[uid] for uid in miner_uids],
-            synapse=TextTo3D(dummy_input=self.step),
+            synapse=TextTo3D(prompt_in=prompt),
             deserialize=False,
         )
 
-        bt.logging.info(f"Received responses: {responses}")
+        bt.logging.info(
+            f"Received {len([r for r in responses if r.mesh_out is not None])} responses"
+        )
 
-        # TODO: move scoring to a separate module
-        scores = self.score_responses(responses=responses)
+        scores = score_responses(prompt, responses, self.device, self.models)
 
         bt.logging.info(f"Scored responses: {scores}")
 
         self.update_scores(scores, miner_uids)
-
-    def score_response(self, synapse: TextTo3D) -> float:
-        if synapse.dummy_output is None:
-            return 0.0
-        return 1.0 if synapse.dummy_output == synapse.dummy_input * 2 else 0.0
-
-    def score_responses(self, responses: List[TextTo3D]) -> torch.FloatTensor:
-        return torch.FloatTensor(
-            [self.score_response(synapse) for synapse in responses]
-        ).to(self.device)
 
 
 def main():
